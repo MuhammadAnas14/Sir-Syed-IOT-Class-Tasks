0cab0d1f8231cd1c681408676a3eb28767a4b285
Anas452 <anasm9877@gmail.com>
2019-10-05 17:13:26 +0500

    chapter3 function

---
+++ chap3 func/src/main.rs
@@ -0,0 +1,21 @@
+// task 1
+
+// fn main() {
+//     let result = add(10,12,13);
+//     println!("the sum is = {}",result);
+// }
+// fn add(x:i32,y:i32,z:i32)-> i32{
+//     x + y + z
+// }
+
+// task 2 
+
+// fn main(){
+//     let result = multiply(3.2,8.2,9.3);
+//     println!("The ans is = {}",result);
+// }
+
+// fn multiply(a:f64,b:f64,c:f64)->f64{
+//     a * b * c
+
+// }
